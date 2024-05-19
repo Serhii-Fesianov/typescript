74d9ff9672432c5c4024e6f494a84e30bba01721
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-05-19 14:45:57 +0300

    test function

--- dist/concatenation.js
+++ dist/concatenation.js
@@ -2,4 +2,11 @@ function concatenation(firstWord, secondWord) {
     console.log(`${firstWord} ${secondWord}`);
 }
 export { concatenation };
+function getStat(stat) {
+    const object = { ...stat, medium: 52 };
+    console.log(object);
+    return object;
+}
+getStat({ noob: 45, pro: 60, expert: 68 });
+getStat({ expert: 70, noob: 38 });
 //# sourceMappingURL=concatenation.js.map
--- src/concatenation.ts
+++ src/concatenation.ts
@@ -3,3 +3,24 @@ function concatenation(firstWord: string, secondWord: string) {
 }
 
 export { concatenation };
+
+interface AnimalWOT {
+  noob: number;
+  pro?: number;
+  expert: number;
+}
+
+interface ExtendedAnimalWot extends AnimalWOT {
+  medium: number;
+}
+
+function getStat(stat: AnimalWOT): ExtendedAnimalWot {
+  const object: ExtendedAnimalWot = { ...stat, medium: 52 };
+  console.log(object);
+
+  return object;
+}
+
+getStat({ noob: 45, pro: 60, expert: 68 });
+getStat({ expert: 70, noob: 38 });
+export {};
